1a32981ddce0923caae95dbddd8c54043f13da6b
Mohammad Suhel <122422109+Suhel03@users.noreply.github.com>
2023-02-05 11:40:57 +0530

    Update index.js
    Added Game Over!

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 var boxs = document.querySelectorAll(".box");
 var turn =0;
-var grid_values =[null,null,null,null,null,null,null,null,null]
-for(var i=0; i<boxs.length;i++){
-    let x = boxs[i];
-    let y = i;
-    
+var grid_values = ["","","","","","","","",""];
+var values = [true,true,true,true,true,true,true,true,true];
+let isGameActive = true;
+var countTurns=0;
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+boxs.forEach(cell => {
+    box.addEventListener('click' ,() => {
+        var index=cell.getAttribute("id");
+        console.log(values[index]);
 
-    x.addEventListener("click", () => {
-        if(turn === 0)
-        x.style.backgroundColor ="red";
-        else
-        x.style.backgroundColor ="blue";
-
-        turn =1-turn;
-        grid_values[y] = turn;
-        console.log(grid_values);
-
-       
-
-        if(isGameOver()){
-
+        if(turn ===0 && values[index])
+        {
+        box.style.backgroundColor="red";
+        values[index]= false;
+        grid_values[index]=0;
+        console.log(grid_values[index]);
+        turn = 1-turn;
+        countTurns+=1;
         }
-    }) 
-}
-//  isGameOver =()=>{
-
-//  }
+        else if (values[index])
+        {
+        box.style.backgroundColor="blue";
+        values[index]=false;
+        grid_values[index]=1;
+        console.log(grid_values[index]);
+        turn = 1-turn;
+        countTurns+=1;
+        }
+        // console.log(box);
+        if(countTurns==9){
+            console.log("Sorry ! Game Over :(")
+        }
+        
+    })
+});
 
 
